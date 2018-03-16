5ed255566115bec555f110120b65d431a6833d1f
cara ottmar <cara.ottmar@gmail.com>
2018-03-16 09:48:38 -0700

    Code Wars March 16th

---
+++ solution.js
@@ -0,0 +1,14 @@
+function capitalize(s){
+  let even = '';
+  let odd = '';
+  for (let i=0; i<s.length; i++){
+    if (i%2){
+      even+=s.charAt(i).toUpperCase();
+      odd+=s.charAt(i).toLowerCase();
+    } else {
+      even+=s.charAt(i).toLowerCase();
+      odd+=s.charAt(i).toUpperCase();
+    }
+  }
+  return [odd, even];
+ };
