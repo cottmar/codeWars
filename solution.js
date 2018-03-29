0824d775337433ffcf378d8707e8a9964ffa50da
cara ottmar <cara.ottmar@gmail.com>
2018-03-29 09:30:08 -0700

    thursday kata

---
+++ solution.js
@@ -0,0 +1,15 @@
+function mutateMyStrings(stringOne, stringTwo) {
+  let stringOneArray = stringOne.split('');
+  let stringTwoArray = stringTwo.split('');
+
+  let result = (stringOne + '\n');
+
+  for (i = 0; i < stringOneArray.length; i++) {
+
+    if (stringOneArray[i] !== stringTwoArray[i]) {
+      stringOneArray[i] = stringTwoArray[i];
+      result = result.concat(stringOneArray.join('') + '\n');
+    }
+  }
+  return result;
+}
